600aadc83141f4ac55eefe65ccf8104316fce67a
devmagalhaes975 <68703989+devmagalhaes975@users.noreply.github.com>
2020-07-23 13:45:42 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <title>Ovos de Páscoa</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <style>
+        body{
+            
+        }
+
+        @media (max-width: 1000px){
+            .whatsapp-link{
+                position: fixed;
+                width: 150px;
+                height: 150px;
+                bottom: 250px;
+                right: 65px;
+                background: #25d366;
+                color: white;
+                border-radius: 80px;
+                text-align: center;
+                font-size: 30px;
+                box-shadow: 1px 1px 2px #888;
+                z-index: 1000;
+            }
+
+            .fa-whatsapp{
+                font-size: 80px;
+                margin-top: 35px;
+            }
+            
+            body{
+                background: blue;
+                margin: 0 0 55px 0;
+            }
+
+            .app-footer{
+                position: fixed;
+                bottom: 0;
+                width: 100%;
+                height: 145px;
+                box-shadow: 0 0 3px rgba(0, 0, 0, 0.2);
+                background-color: #ffffff;
+                display: flex;
+                overflow-x: auto;
+                font-size: 35px;
+                font-family: sans-serif;
+            }
+
+            .app-footer ul{
+                display: flex;
+                align-items: center;
+                justify-content: space-around;
+                list-style: none;
+            }
+
+            .app-footer ul li{
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                padding: 0 60px;
+            }
+
+            .app-footer ul li i{
+                font-size: 70px;
+            }
+        }
+    </style>
+  </head>
+  <body>
+        <a class="whatsapp-link" href="https://web.whatsapp.com/send?phone=559985019439" target="_blank">
+            <i class="fa fa-whatsapp" aria-hidden="true"></i>
+        </a>
+    <footer class="app-footer">
+        <ul>
+            <li>
+            <i class="fa fa-heart-o" aria-hidden="true"></i>
+            HOME
+            </li>
+            <li>
+            <i class="fa fa-heart" aria-hidden="true"></i>
+            TRUFAS
+            </li>
+            <li>
+            <i class="fa fa-heart" aria-hidden="true"></i>
+            OVOS
+            </li>
+            <li>
+            <i class="fa fa-heart-o" aria-hidden="true"></i>
+            DIMDIM
+            </li>
+        </ul>
+    </footer>
+  <?php
+  
+  ?>
+  </body>
+</html>
